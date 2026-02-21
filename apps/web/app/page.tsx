cf28167d33bf5f46d10b199f2a86f665537934e6
Aman Karn <amankarn2007@gmail.com>
2026-02-21 09:22:01 +0530

    frontent

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,9 +2,9 @@
 
 export default function Home() {
   return (
-    <div className="" style={{width:"100%", height: "100vh", display: "flex", alignItems: "center", justifyContent: "center"}}>
+    <div className="" style={{width:"100%", height: "100vh", display: "flex", alignItems: "center", justifyContent: "center", backgroundColor: "#f3f4f6"}}>
 
-      <div style={{width: "480px", height: "620px", background: "pink", borderRadius: "12px", display: "flex", justifyContent: "center", alignItems: "center"}}>
+      <div style={{width: "480px", height: "620px", background: "pink", borderRadius: "12px", display: "flex", justifyContent: "center", alignItems: "center", borderColor: "gray", boxShadow: "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",}}>
         Signup Page
       </div>
     </div>
