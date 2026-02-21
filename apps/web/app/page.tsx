891b37c591c266f8b5274b734bd09c09137f001e
Aman Karn <amankarn2007@gmail.com>
2026-02-21 09:09:41 +0530

    frontent

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="" style={{width:"full", height: "screen", background: "blue"}}>
       <div style={{width: "80", height: "120", background: "pink"}}>
-
+        helllo
       </div>
     </div>
   );
